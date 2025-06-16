3b8d8631b91e685d87402864daf8655b73e31da6
Mamet Ucup <aliminthekings@gmail.com>
2025-06-16 23:14:31 +0700

    Create script.js

---
+++ script.js
@@ -0,0 +1,4 @@
+// This script is for basic interactions (optional)
+document.querySelector('.cta-button').addEventListener('click', function() {
+  alert('Redirecting to the games section...');
+});
