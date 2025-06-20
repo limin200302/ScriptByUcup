b33765dd196b16eea9dcf8111d01e887659503c3
Mamet Ucup <aliminthekings@gmail.com>
2025-06-20 23:18:16 +0700

    Update script.js

--- script.js
+++ script.js
@@ -78,3 +78,7 @@ document.addEventListener('DOMContentLoaded', () => {
   // Tampilkan slide pertama
   showSlide(currentSlide);
 });
+function toggleMenu() {
+  const menu = document.getElementById('mobile-menu');
+  menu.classList.toggle('show');
+}
