af2718bb36ba96c969db6c42f200bba99e4d42c0
Mamet Ucup <aliminthekings@gmail.com>
2025-06-21 07:25:18 +0700

    Update login.js

--- login.js
+++ login.js
@@ -1,45 +1,20 @@
-import { initializeApp } from "https://www.gstatic.com/firebasejs/11.10.0/firebase-app.js";
-import { getAuth, signInWithPopup, GoogleAuthProvider, onAuthStateChanged } from "https://www.gstatic.com/firebasejs/11.10.0/firebase-auth.js";
+import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm';
 
-// Konfigurasi Firebase kamu
-const firebaseConfig = {
-  apiKey: "AIzaSyA-29lXZhj3j8xHZTyGHhhxjFWlc4Yi9xc",
-  authDomain: "mamet-ucup-store.firebaseapp.com",
-  projectId: "mamet-ucup-store",
-  storageBucket: "mamet-ucup-store.appspot.com",
-  messagingSenderId: "869665931228",
-  appId: "1:869665931228:web:2e4aac081befbca516d919",
-  measurementId: "G-VR803Q47EC"
-};
+const supabase = createClient(
+  'https://etfbdevjytilaykogzwa.supabase.co',
+  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImV0ZmJkZXZqeXRpbGF5a29nendhIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTA0NjE0MjAsImV4cCI6MjA2NjAzNzQyMH0.rGwSOp2_l9eWK2B7Fk7BFo0_JK4BOY5GAYJOa3C58tM'
+);
 
-// Inisialisasi Firebase
-const app = initializeApp(firebaseConfig);
-const auth = getAuth(app);
-
-// Login dengan Google
+// === Tombol login Google
 const googleBtn = document.getElementById("googleLogin");
 
 googleBtn.addEventListener("click", async () => {
-  const provider = new GoogleAuthProvider();
-  try {
-    const result = await signInWithPopup(auth, provider);
-    const user = result.user;
+  const { error } = await supabase.auth.signInWithOAuth({
+    provider: 'google',
+    options: {
+      redirectTo: "https://aesthetic-crostata-7c8181.netlify.app/index.html"
+    }
+  });
 
-    // Simpan nama user ke localStorage (jika kamu mau tampilkan di index.html)
-    localStorage.setItem("username", user.displayName);
-
-    // Redirect ke index.html
-    window.location.href = "index.html";
-  } catch (error) {
-    alert("Login gagal: " + error.message);
-  }
-});
-
-// (Optional) Cek status login saat file dimuat
-onAuthStateChanged(auth, (user) => {
-  if (user) {
-    console.log("Sudah login sebagai:", user.displayName);
-  } else {
-    console.log("Belum login");
-  }
+  if (error) alert("Login gagal: " + error.message);
 });
