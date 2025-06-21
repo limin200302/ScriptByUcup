113f410049f326648913489a3c48faaae70e7c48
Mamet Ucup <aliminthekings@gmail.com>
2025-06-21 08:01:30 +0700

    Update login.js

--- login.js
+++ login.js
@@ -1,7 +1,7 @@
 import { createClient } from "https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm";
 
 const supabaseUrl = "https://etfbdevjytilaykogzwa.supabase.co";
-const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9..." // gunakan API key lengkap di file asli
+const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImV0ZmJkZXZqeXRpbGF5a29nendhIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTA0NjE0MjAsImV4cCI6MjA2NjAzNzQyMH0.rGwSOp2_l9eWK2B7Fk7BFo0_JK4BOY5GAYJOa3C58tM" // gunakan API key lengkap di file asli
 const supabase = createClient(supabaseUrl, supabaseKey);
 
 const form = document.getElementById("loginForm");
